2baa036f5aff1410ea4e275e43dd6ea43bc98c31
MananSharma12 <manan.sharma1205@gmail.com>
2025-09-20 09:50:01 +0530

    feat: add ERC721 import to Card contract

--- contracts/src/Card.sol
+++ contracts/src/Card.sol
@@ -1,6 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import {ERC721Enumerable} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 
 contract Card is ERC721Enumerable {
